5c182061d07d96427b0be3dd157377ffaadfb677
PATANJAMSHEER <Jamsheerkhan118@email.com>
2026-02-28 06:37:40 +0000

    switch to brevo smtp

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app.secret_key = "campus_secret_key_2024"
 
 # ── Email Config ─────────────────────────────────────────────
 # ⚠️  CHANGE THESE 2 LINES with your Gmail and App Password
-app.config['MAIL_SERVER']         = 'smtp.gmail.com'
-app.config['MAIL_PORT']           = 465
-app.config['MAIL_USE_TLS']        = False
-app.config['MAIL_USE_SSL']         = True
+app.config['MAIL_SERVER']         = os.environ.get('MAIL_SERVER', 'smtp-relay.brevo.com')
+app.config['MAIL_PORT']           = int(os.environ.get('MAIL_PORT', 587))
+app.config['MAIL_USE_TLS']        = True
+app.config['MAIL_USE_SSL']         = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'jamsheerkhan118@gmail.com')     # ← PUT YOUR GMAIL HERE
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')  # ← PUT YOUR APP PASSWORD HERE
 app.config['MAIL_DEFAULT_SENDER'] = 'jamsheerkhan118@gmail.com'     # ← SAME GMAIL HERE
