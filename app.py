b85ec6b909cef175fa15ba85ad13bcbb65674c5b
PATANJAMSHEER <Jamsheerkhan118@email.com>
2026-02-28 06:45:50 +0000

    fix mail username

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ app.config['MAIL_SERVER']         = os.environ.get('MAIL_SERVER', 'smtp-relay.br
 app.config['MAIL_PORT']           = int(os.environ.get('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS']        = True
 app.config['MAIL_USE_SSL']         = False
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'jamsheerkhan118@gmail.com')     # ← PUT YOUR GMAIL HERE
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')  # ← PUT YOUR APP PASSWORD HERE
-app.config['MAIL_DEFAULT_SENDER'] = 'jamsheerkhan118@gmail.com'     # ← SAME GMAIL HERE
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 
 mail = Mail(app)
 
