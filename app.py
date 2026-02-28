4f7ec09c5709c382c60cb3e22b8ff2e275782899
PATANJAMSHEER <Jamsheerkhan118@email.com>
2026-02-28 05:33:31 +0000

    update mail password

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['MAIL_SERVER']         = 'smtp.gmail.com'
 app.config['MAIL_PORT']           = 587
 app.config['MAIL_USE_TLS']        = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'jamsheerkhan118@gmail.com')     # ← PUT YOUR GMAIL HERE
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'ugyr lwyb fgvq rpwb')  # ← PUT YOUR APP PASSWORD HERE
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'iecp zpbj vzof zumd')  # ← PUT YOUR APP PASSWORD HERE
 app.config['MAIL_DEFAULT_SENDER'] = 'jamsheerkhan118@gmail.com'     # ← SAME GMAIL HERE
 
 mail = Mail(app)
