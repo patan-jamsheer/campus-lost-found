7bcaa0623e3f9c4d9cca388944bdc935c7af02d1
PATANJAMSHEER <Jamsheerkhan118@email.com>
2026-02-28 05:25:41 +0000

    use PORT env variable

--- app.py
+++ app.py
@@ -744,4 +744,4 @@ def delete_found_item(item_id, user_id):
 
 # ════════════════════════════════════════════════════════════
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
